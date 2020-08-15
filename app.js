22790a7d1212e7c7adc2462bb7229b2a40733f2e
Emir Kaynar <mstfemirkynr@protonmail.com>
2020-08-15 17:21:04 +0300

    improved image loading with lazy loading

---
+++ app.js
@@ -0,0 +1,24 @@
+const targets = document.querySelectorAll('#shown');
+
+const lazyLoad = target => {
+  const io = new IntersectionObserver((entries, observer) => {
+    console.log(entries)
+    entries.forEach(entry => {
+      console.log('😍');
+
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-lazy');
+
+        img.setAttribute('src', src);
+        img.classList.add('fade');
+
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target)
+};
+
+targets.forEach(lazyLoad);

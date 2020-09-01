07e18651b6815ce6af5e75f266a2e555ef9540d4
Emir Kaynar <mstfemirkynr@protonmail.com>
2020-09-01 22:57:39 +0300

    Version 200965a (Check changelog.txt)

---
+++ local/script/variable.js
@@ -0,0 +1,11 @@
+var websiteVersionVar = 'Version 200965a' ;
+    
+  document.getElementById("websiteVersion").innerHTML = websiteVersionVar ;
+
+var gridCountVar = '22' ;
+    
+  document.getElementById("gridCount").innerHTML = gridCountVar ;
+
+var materialCountVar = '0' ;
+    
+  document.getElementById("materialCount").innerHTML = materialCountVar ;

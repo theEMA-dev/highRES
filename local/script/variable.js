f1f1a84c386cc8ec5098a619df659ef2e0ce3009
Emir Kaynar <mstfemirkynr@protonmail.com>
2020-09-02 19:15:19 +0300

    Version 200965f *cdnsync1

--- local/script/variable.js
+++ local/script/variable.js
@@ -1,8 +1,14 @@
+/* VERSION TAMPLATE 
+ Version 20 = year
+         09 = month
+         65 = commit id
+         a = alpha (or) b = beta (or) f = final
+*/
 var websiteVersionVar = 'Version 200965f' ;
     
   document.getElementById("websiteVersion").innerHTML = websiteVersionVar ;
 
-var gridCountVar = '24' ;
+var gridCountVar = '27' ;
     
   document.getElementById("gridCount").innerHTML = gridCountVar ;
 

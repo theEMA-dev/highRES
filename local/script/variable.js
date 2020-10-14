af44b9013fe0856b159d5f9dfc061c46199a1219
Emir Kaynar <mstfemirkynr@protonmail.com>
2020-10-14 21:01:36 +0300

    Version 200972f *cdnsync1

--- local/script/variable.js
+++ local/script/variable.js
@@ -8,7 +8,7 @@ var websiteVersionVar = 'Version 200972f' ;
     
   document.getElementById("websiteVersion").innerHTML = websiteVersionVar ;
 
-var gridCountVar = '31' ;
+var gridCountVar = '32' ;
     
   document.getElementById("gridCount").innerHTML = gridCountVar ;
 

bb27e30d9887a5296478a60072d1d72f6ac4a9b4
Emir Kaynar <mstfemirkynr@protonmail.com>
2020-10-17 01:44:27 +0300

    Version 201082a (Image viewer added)

--- local/script/variable.js
+++ local/script/variable.js
@@ -4,7 +4,7 @@
          65 = commit id
          a = alpha (or) b = beta (or) f = final
 */
-var websiteVersionVar = 'Version 200972f' ;
+var websiteVersionVar = 'Version 201082a' ;
     
   document.getElementById("websiteVersion").innerHTML = websiteVersionVar ;
 

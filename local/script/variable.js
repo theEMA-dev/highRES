9daabc0ff695e6a3c86ac98127fde95a95dadb75
Emir Kaynar <mstfemirkynr@protonmail.com>
2020-12-22 18:17:14 +0300

    Versiob 201188 # 2020 Final Update

--- local/script/variable.js
+++ local/script/variable.js
@@ -8,7 +8,7 @@ var websiteVersionVar = 'Version 201188f' ;
     
   document.getElementById("websiteVersion").innerHTML = websiteVersionVar ;
 
-var gridCountVar = '66' ;
+var gridCountVar = '67' ;
     
   document.getElementById("gridCount").innerHTML = gridCountVar ;
 

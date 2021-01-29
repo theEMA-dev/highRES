91e01aaba0c8e5fd5049ff8b83cdcb21b90f4615
Emir Kaynar <mstfemirkynr@protonmail.com>
2021-01-29 10:20:54 +0300

    Version 2.0.8

--- local/script/variable.js
+++ local/script/variable.js
@@ -9,6 +9,10 @@
           01 = month
           a,b,f = alpha,beta,final
 */
+var alertTitle = document.querySelectorAll(".alert");
+
+  alertTitle[0].title = 'Uncategorized: 30\nMap Labs Collection: 52\nLEGO Collection: 8\nSoftware Collection: 10'
+
 var websiteVersionVar = 'Version 2.0.8' ;
     
   document.getElementById("websiteVersion").innerHTML = websiteVersionVar ;
@@ -16,6 +20,10 @@ var websiteVersionVar = 'Version 2.0.8' ;
 var gridCountVar = '100' ;
     
   document.getElementById("gridCount").innerHTML = gridCountVar ;
+
+var gridCategoriesVar = 'Uncategorized: 30\nMap Labs Collection: 52\nLEGO Collection: 8\nSoftware Collection: 10' ;
+
+  document.getElementById("gridCount").title = gridCategoriesVar ;
 
 var materialCountVar = '0' ;
     

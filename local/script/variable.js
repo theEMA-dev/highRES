2317f46e4f3528556ad8b9210873621646e0efe0
Emir Kaynar <mstfemirkynr@protonmail.com>
2021-03-26 19:27:59 +0300

    Version 2.1.1

--- local/script/variable.js
+++ local/script/variable.js
@@ -1,4 +1,4 @@
-/* VERSION TEMPLATE 
+/* VERSION TEMPLATE 🥳
  Version 2.0.3-2101a
          2 = major version
          0 = minor version 
@@ -10,25 +10,15 @@
           a,b,f = alpha,beta,final
 */
 var alertTitle = document.querySelectorAll(".alert");
+  alertTitle[0].title = 'placeholder' ;
+  alertTitle[0].style.display = 'none' ;
+  alertTitle[0].innerHTML = '<p>placeholder</p>' ;
 
-  alertTitle[0].title = 'Uncategorized: 30\nMap Labs Collection: 54\nLEGO Collection: 9\nSoftware Collection: 10'
-
-var websiteVersionVar = 'Version 2.0.8' ;
-    
-  document.getElementById("websiteVersion").innerHTML = websiteVersionVar ;
-
-var gridCountVar = '103' ;
-    
-  document.getElementById("gridCount").innerHTML = gridCountVar ;
-
-var gridCategoriesVar = 'Uncategorized: 30\nMap Labs Collection: 54\nLEGO Collection: 8\nSoftware Collection: 10' ;
-
-  document.getElementById("gridCount").title = gridCategoriesVar ;
-
-var materialCountVar = '0' ;
-    
-  document.getElementById("materialCount").innerHTML = materialCountVar ;
-
+  document.getElementById("websiteVersion").innerHTML = 'Version 2.1.3' ;
+  document.getElementById("gridCount").innerHTML = '104' ;
+  document.getElementById("gridCount").title = 'Uncategorized: 30\nMap Labs Collection: 55\nLEGO Collection: 8\nSoftware Collection: 10' ;
+  document.getElementById("materialCount").innerHTML = '0' ;
+  
 var badgeAddSelect = document.querySelectorAll(".bd-add");
   for (var i = 0; i <= badgeAddSelect.length - 1; i++) {
     badgeAddSelect[i].textContent = 'ADD';
@@ -46,4 +36,4 @@ var badgeChangeSelect = document.querySelectorAll(".bd-change");
     badgeChangeSelect[i].textContent = 'CHANGE';
   }
 a = document.getElementById("websiteVersion");
-a.setAttribute("href", "https://theema-dev.github.io/highRES/help#version2.0");
+a.setAttribute("href", "https://theema-dev.github.io/highRES/help#version2.1");
